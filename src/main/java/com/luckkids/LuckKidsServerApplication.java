b4ac3e01952ec6cf9155d11589bbfe1d8827ad64
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-04 10:25:54 +0900

    feat: Start Server

---
+++ src/main/java/com/luckkids/LuckKidsServerApplication.java
@@ -0,0 +1,13 @@
+package com.luckkids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckKidsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckKidsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luckkids/LuckKidsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luckkids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckKidsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
