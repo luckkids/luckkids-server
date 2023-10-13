eb97efbf1a8755493f8662224f03041369c06d58
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-13 13:44:20 +0900

    feat: restDocs에 잘뜨는지 확인용 TestController

---
+++ src/main/java/com/luckkids/api/controller/dto/TestRequest.java
@@ -0,0 +1,12 @@
+package com.luckkids.api.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TestRequest {
+
+    private String test1;
+    private String test2;
+}
---
+++ src/main/java/com/luckkids/api/controller/dto/TestResponse.java
@@ -0,0 +1,10 @@
+package com.luckkids.api.controller.dto;
+
+import lombok.Getter;
+
+@Getter
+public class TestResponse {
+
+    private String test1;
+    private String test2;
+}
