d6fca882c6026e7420044edbbd704b58c46cc480
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-16 12:32:50 +0900

    feat: 회원가입 테스트코드 추가

--- src/test/java/com/luckkids/join/JoinServiceTest.java
+++ src/test/java/com/luckkids/join/JoinServiceTest.java
@@ -2,18 +2,15 @@ package com.luckkids.join;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.luckkids.api.service.join.JoinService;
-import com.luckkids.api.service.join.dto.JoinResponse;
 import com.luckkids.api.service.join.dto.JoinSendMailServiceRequest;
 import com.luckkids.api.service.join.dto.JoinServiceRequest;
-import com.luckkids.domain.user.SnsType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -59,28 +56,20 @@ class JoinServiceTest {
     }
 
     @Test
+    @Rollback(value = false)
     void saveUser() throws  Exception{
-        JoinServiceRequest joinRequest = JoinServiceRequest.builder()
+        JoinServiceRequest request = JoinServiceRequest.builder()
                 .email("tkdrl8908@naver.com")
                 .nickname("상기")
                 .password("1234")
                 .phoneNumber("01064091048")
                 .build();
-
-        JoinResponse joinResponse = JoinResponse.builder()
-                .email("tkdrl8908@naver.com")
-                .nickname("상기")
-                .password("1234")
-                .phoneNumber("01064091048")
-                .snsType(SnsType.NORMAL.getText())
-                .build();
-
-        given(joinService.joinUser(any(JoinServiceRequest.class)));
 
         mockMvc.perform(
                         post("/api/v1/join/user")
-                                .content(objectMapper.writeValueAsString(joinRequest))
+                                .content(objectMapper.writeValueAsString(request))
                                 .contentType(APPLICATION_JSON)
+                                .accept(APPLICATION_JSON)
                 )
                 .andDo(print())
                 .andExpect(status().isOk())
--- src/test/java/com/luckkids/join/docs/JoinControllerDocTest.java
+++ src/test/java/com/luckkids/join/docs/JoinControllerDocTest.java
@@ -2,13 +2,17 @@ package com.luckkids.join.docs;
 
 import com.luckkids.AbstractRestDocsTests;
 import com.luckkids.api.controller.join.JoinController;
+import com.luckkids.api.controller.join.dto.JoinRequest;
 import com.luckkids.api.service.join.JoinService;
+import com.luckkids.api.service.join.dto.JoinResponse;
 import com.luckkids.api.service.join.dto.JoinSendMailResponse;
 import com.luckkids.api.service.join.dto.JoinSendMailServiceRequest;
+import com.luckkids.api.service.join.dto.JoinServiceRequest;
+import com.luckkids.domain.user.SnsType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.restdocs.payload.JsonFieldType;
 
 import static org.mockito.ArgumentMatchers.any;
@@ -24,7 +28,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(JoinController.class)
 public class JoinControllerDocTest extends AbstractRestDocsTests {
 
-    private final JoinService joinService = Mockito.mock(JoinService.class);
+    @MockBean
+    private JoinService joinService;
+
 
     @DisplayName("이메일 인증코드 전송 API")
     @Test
@@ -60,4 +66,62 @@ public class JoinControllerDocTest extends AbstractRestDocsTests {
                 ));
     }
 
+    @DisplayName("회원가입 API")
+    @Test
+    void joinUser() throws Exception {
+        JoinRequest request = JoinRequest.builder()
+                .email("tkdrl8908@naver.com")
+                .nickname("상기")
+                .password("1234")
+                .phoneNumber("01064091048")
+                .build();
+
+        given(joinService.joinUser(any(JoinServiceRequest.class)))
+                .willReturn(JoinResponse.builder()
+                        .email("tkdrl8908@naver.com")
+                        .nickname("상기")
+                        .password("1234")
+                        .phoneNumber("01064091048")
+                        .snsType(SnsType.NORMAL)
+                        .build()
+                );
+
+        mockMvc.perform(
+                        post("/api/v1/join/user")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andDo(
+                    restDocs.document(
+                        requestFields(
+                                fieldWithPath("email").type(JsonFieldType.STRING)
+                                        .description("이메일"),
+                                fieldWithPath("password").type(JsonFieldType.STRING)
+                                        .description("비밀번호"),
+                                fieldWithPath("nickname").type(JsonFieldType.STRING)
+                                        .description("사용자 별칭"),
+                                fieldWithPath("phoneNumber").type(JsonFieldType.STRING)
+                                        .description("사용자 전화번호")
+                        ),
+                        responseFields(
+                                fieldWithPath("statusCode").type(JsonFieldType.NUMBER)
+                                        .description("API상태코드"),
+                                fieldWithPath("httpStatus").type(JsonFieldType.STRING)
+                                        .description("API상태"),
+                                fieldWithPath("message").type(JsonFieldType.STRING)
+                                        .description("메시지"),
+                                fieldWithPath("data.email").type(JsonFieldType.STRING)
+                                        .description("이메일"),
+                                fieldWithPath("data.password").type(JsonFieldType.STRING)
+                                        .description("비밀번호"),
+                                fieldWithPath("data.nickname").type(JsonFieldType.STRING)
+                                        .description("사용자 별칭"),
+                                fieldWithPath("data.phoneNumber").type(JsonFieldType.STRING)
+                                        .description("사용자 전화번호"),
+                                fieldWithPath("data.snsType").type(JsonFieldType.STRING)
+                                        .description("사용자 로그인 타입")
+                        )
+                ));
+    }
 }
