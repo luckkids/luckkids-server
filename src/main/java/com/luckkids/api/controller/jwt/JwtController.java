17a0b89c3e372fe5c7efeffe4a5b6548a0472ad7
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-16 14:47:01 +0900

    feat: jwt토큰 생성 및 테스트Controller

---
+++ src/main/java/com/luckkids/api/controller/jwt/JwtController.java
@@ -0,0 +1,43 @@
+package com.luckkids.api.controller.jwt;
+
+import com.luckkids.config.login.LoginUser;
+import com.luckkids.jwt.JwtTokenGenerator;
+import com.luckkids.jwt.dto.JwtToken;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/jwt")
+public class JwtController {
+
+    private final JwtTokenGenerator jwtTokenGenerator;
+
+    /*
+     * 토큰 발급 API
+     * */
+    @PostMapping("/token")
+    public JwtToken getToken(String email) {
+        return jwtTokenGenerator.generate(email);
+    }
+
+    /*
+     * accesstoken 갱신 API
+     * */
+    @PostMapping("/refresh")
+    public JwtToken refreshToken(String refreshToken) {
+        return jwtTokenGenerator.generateAccessToken(refreshToken);
+    }
+
+    /*
+     * token 유효 테스트
+     * */
+    @PostMapping("/test")
+    public String test(@LoginUser String email) {
+        return email;
+    }
+
+}
+
