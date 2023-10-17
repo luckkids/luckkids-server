5a58246a4f25a6b5653d0deb4dfda681b0a76fe0
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-17 17:10:16 +0900

    refactor: 코드 스타일 맞추기

--- src/main/java/com/luckkids/api/controller/join/JoinController.java
+++ src/main/java/com/luckkids/api/controller/join/JoinController.java
@@ -1,35 +1,38 @@
 package com.luckkids.api.controller.join;
 
 import com.luckkids.api.ApiResponse;
-import com.luckkids.api.controller.join.dto.*;
+import com.luckkids.api.controller.join.request.*;
 import com.luckkids.api.service.join.JoinService;
-import com.luckkids.api.service.join.dto.JoinResponse;
-import com.luckkids.api.service.join.dto.JoinSendMailResponse;
+import com.luckkids.api.service.join.response.JoinCheckEmailResponse;
+import com.luckkids.api.service.join.response.JoinResponse;
+import com.luckkids.api.service.join.response.JoinSendMailResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/join")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/join")
 public class JoinController {
 
     private final JoinService joinService;
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/checkEmail")
-    public void checkEmail(@RequestBody JoinCheckEmailRequest joinCheckEmailRequest){
-        joinService.checkEmail(joinCheckEmailRequest.toServiceRequest());
+    public ApiResponse<JoinCheckEmailResponse> checkEmail(@Valid  @RequestBody JoinCheckEmailRequest joinCheckEmailRequest){
+        return ApiResponse.ok(joinService.checkEmail(joinCheckEmailRequest.toServiceRequest()));
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @PostMapping("/sendMail")
-    public ApiResponse<JoinSendMailResponse> sendEmail(@RequestBody JoinSendMailRequest joinSendMailRequest){
+    public ApiResponse<JoinSendMailResponse> sendEmail(@Valid @RequestBody JoinSendMailRequest joinSendMailRequest){
         return ApiResponse.ok(joinService.sendMail(joinSendMailRequest.toServiceRequest()));
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/user")
-    public ApiResponse<JoinResponse> joinUser(@RequestBody JoinRequest joinRequest){
-        return ApiResponse.ok(joinService.joinUser(joinRequest.toServiceRequest()));
+    public ApiResponse<JoinResponse> joinUser(@Valid @RequestBody JoinRequest joinRequest){
+        return ApiResponse.created(joinService.joinUser(joinRequest.toServiceRequest()));
     }
 }
