320c72219f97ab2d8b9ba57c42a329faabc71049
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-17 17:09:25 +0900

    delete: 기존 UserRepository 제거

--- src/main/java/com/luckkids/api/repository/join/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luckkids.api.repository.join;
-
-import com.luckkids.domain.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    public User findByEmail(String email);
-}
