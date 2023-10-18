0b6bca76a75c10a39a99568fe27f801e76abd2fa
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-18 10:03:03 +0900

    fix: 회원가입 로그인을 제외한 API는 ROLE_USER 권한체크

--- src/main/java/com/luckkids/config/SecurityConfig.java
+++ src/main/java/com/luckkids/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,7 +22,7 @@ public class SecurityConfig {
     private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(AbstractHttpConfigurer::disable)
 
@@ -31,13 +30,16 @@ public class SecurityConfig {
 
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(
+                        new AntPathRequestMatcher("/api/v1/jwt/**"),
+                        new AntPathRequestMatcher("/api/v1/auth/**"),
+                        new AntPathRequestMatcher("/api/v1/join/**"),
                         new AntPathRequestMatcher("/css/**"),
                         new AntPathRequestMatcher("/images/**"),
                         new AntPathRequestMatcher("/js/**"),
                         new AntPathRequestMatcher("/h2-console/**"),
                         new AntPathRequestMatcher("/health-check")
                 ).permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().hasAnyAuthority("ROLE_USER")
             )
 
             .logout((logout) -> logout
@@ -47,21 +49,6 @@ public class SecurityConfig {
             .addFilterBefore(new JwtExceptionHandlerFilter(), JwtAuthenticationFilter.class); //JwtAuthenticationFilter에서 뱉은 에러를 처리하기 위한 Filter
 
         return http.build();
-    }
-
-    /*
-    * JwtFilter 제외 API 지정
-    * permitAll이 모든 요청 처리 과정에서 적용되는 모든 필터들을 무시하지않는다고 한다.
-    * 참고: https://velog.io/@refoli_20/Spring-Security-permitAll-Filter-%ED%98%B8%EC%B6%9C
-    * */
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring()
-                .requestMatchers(
-                        "/api/v1/jwt/**",   //jwt토큰 발급 테스트 API
-                        "/api/v1/join/**",  //회원가입 API
-                        "/api/v1/auth/**"   //인증 API
-                );
     }
 }
 
