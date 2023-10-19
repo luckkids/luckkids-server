8c7276c93e6b8c55ce74b5a97cc3ca4f00e76065
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-19 15:46:13 +0900

    fix: message로도 받을 수 있게 수정

--- src/main/java/com/luckkids/api/exception/LuckKidsException.java
+++ src/main/java/com/luckkids/api/exception/LuckKidsException.java
@@ -3,17 +3,20 @@ package com.luckkids.api.exception;
 import lombok.Getter;
 
 @Getter
-
 public class LuckKidsException extends RuntimeException {
 
-    private final ErrorCode errorCode;
+    private final String message;
 
     public LuckKidsException(ErrorCode errorCode, Exception e) {
         super(errorCode.getMessage(), e);
-        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
     }
 
     public LuckKidsException(ErrorCode errorCode) {
-        this.errorCode = errorCode;
+        this.message = errorCode.getMessage();
+    }
+
+    public LuckKidsException(String message){
+        this.message = message;
     }
 }
