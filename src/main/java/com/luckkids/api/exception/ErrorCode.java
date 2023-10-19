ac0973ee886edb2df160ac2d1f61cb864c70e5e2
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-19 15:45:51 +0900

    refractor: @RequiredArgsConstructor로 변경

--- src/main/java/com/luckkids/api/exception/ErrorCode.java
+++ src/main/java/com/luckkids/api/exception/ErrorCode.java
@@ -1,8 +1,10 @@
 package com.luckkids.api.exception;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
 
     USER_UNKNOWN( "해당 사용자가 존재하지 않습니다"),
@@ -18,7 +20,4 @@ public enum ErrorCode {
 
     private final String message;
 
-    private ErrorCode(String message) {
-        this.message = message;
-    }
 }
