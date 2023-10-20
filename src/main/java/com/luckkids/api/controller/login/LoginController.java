7a252e0f1bd420e04ce0145d579274c49fed4fc7
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-20 09:08:54 +0900

    refactor: style변경

--- src/main/java/com/luckkids/api/controller/login/LoginController.java
+++ src/main/java/com/luckkids/api/controller/login/LoginController.java
@@ -1,10 +1,11 @@
 package com.luckkids.api.controller.login;
 
 import com.luckkids.api.ApiResponse;
-import com.luckkids.api.controller.login.dto.LoginOauthRequest;
-import com.luckkids.api.controller.login.dto.LoginRequest;
-import com.luckkids.api.controller.login.dto.LoginResponse;
+import com.luckkids.api.controller.login.request.LoginOauthRequest;
+import com.luckkids.api.controller.login.request.LoginRequest;
 import com.luckkids.api.service.login.LoginService;
+import com.luckkids.api.service.login.response.LoginResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,19 +13,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
 public class LoginController {
 
 	private final LoginService loginService;
 
-	/*
-	* 일반로그인
-	* */
 	@PostMapping("/login")
-	public ApiResponse<LoginResponse> login(@RequestBody LoginRequest loginRequest){
-		LoginResponse loginResponse = loginService.login(loginRequest.toServiceRequest()).toRequest();
-		return ApiResponse.ok(loginResponse);
+	public ApiResponse<LoginResponse> login(@Valid @RequestBody LoginRequest loginRequest){
+		return ApiResponse.ok(loginService.login(loginRequest.toServiceRequest()));
 	}
 
 	@PostMapping("/oauth/login")
