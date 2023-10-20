e4f777382d3c1f1817c83342ed6d9b6f12a9769d
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-20 09:10:27 +0900

    feat: 로그인로직

--- src/main/java/com/luckkids/api/service/login/LoginService.java
+++ src/main/java/com/luckkids/api/service/login/LoginService.java
@@ -2,45 +2,43 @@ package com.luckkids.api.service.login;
 
 import com.luckkids.api.exception.ErrorCode;
 import com.luckkids.api.exception.LuckKidsException;
-import com.luckkids.api.repository.login.LoginRepository;
-import com.luckkids.api.repository.refreshToken.RefreshTokenRepository;
-import com.luckkids.api.service.login.dto.LoginServiceRequest;
-import com.luckkids.api.service.login.dto.LoginServiceResponse;
+import com.luckkids.api.service.login.request.LoginServiceRequest;
+import com.luckkids.api.service.login.response.LoginResponse;
 import com.luckkids.domain.user.User;
-import com.luckkids.security.JwtTokenGenerator;
-import com.luckkids.security.dto.JwtToken;
+import com.luckkids.domain.user.UserRepository;
+import com.luckkids.jwt.JwtTokenGenerator;
+import com.luckkids.jwt.dto.JwtToken;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class LoginService {
 
-    private final LoginRepository loginRepository;
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
     private final JwtTokenGenerator jwtTokenGenerator;
 
-    public LoginServiceResponse login(LoginServiceRequest loginServiceRequest){
-        //1. 회원조회
-        User user = loginRepository.findByEmail(loginServiceRequest.getEmail())
-                .orElseThrow(() -> new LuckKidsException(ErrorCode.USER_UNKNOWN));
+    public LoginResponse login(LoginServiceRequest loginServiceRequest){
+        User user = userRepository.findByEmail(loginServiceRequest.getEmail());     //1. 회원조회
+        Optional.ofNullable(user).orElseThrow(() -> new LuckKidsException(ErrorCode.USER_UNKNOWN));
 
-        //2. SNS가입여부확인
-        user.checkSnsType();
+        user.checkSnsType();                                                        //2. SNS가입여부확인
 
-        //3. 비밀번호 체크 -> 암호화는 추후 추가예정
-        user.checkPassword(loginServiceRequest.getPassword());
+        user.checkPassword(loginServiceRequest.getPassword());                      //3. 비밀번호 체크 -> 암호화는 추후 추가예정
 
-        //4. JWT토큰생성
-        JwtToken jwtToken = jwtTokenGenerator.generate(user.getEmail());
+        JwtToken jwtToken = jwtTokenGenerator.generate(user.getEmail());            //4. JWT토큰생성
 
-        //5. deviceId로 기존 refreshToken 조회 후 수정 혹은 등록
-        String deviceId = loginServiceRequest.getDeviceId();
+        String deviceId = loginServiceRequest.getDeviceId();                        //5. deviceId로 기존 refreshToken 조회 후 수정 혹은 등록
         user.checkRefreshToken(jwtToken, deviceId);
 
-        return new LoginServiceResponse(user, jwtToken);
+        String pushKey = loginServiceRequest.getPushKey();                          //6. deviceId로 기존 push 조회 후 수정 혹은 등록
+        user.checkPushKey(pushKey, deviceId);
+
+        return LoginResponse.of(user, jwtToken);
     }
 
 }
