818ed5d3bdc87c1cece07b3d51573d93150fcbef
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-20 09:09:54 +0900

    feat: refreshtoken pushtoken deviceid와 일치하는지 체크 메소드

--- src/main/java/com/luckkids/domain/user/User.java
+++ src/main/java/com/luckkids/domain/user/User.java
@@ -2,9 +2,10 @@ package com.luckkids.domain.user;
 
 import com.luckkids.api.exception.ErrorCode;
 import com.luckkids.api.exception.LuckKidsException;
-import com.luckkids.domain.refreshToken.RefreshToken;
-import com.luckkids.security.dto.JwtToken;
 import com.luckkids.domain.BaseTimeEntity;
+import com.luckkids.domain.push.Push;
+import com.luckkids.domain.refreshToken.RefreshToken;
+import com.luckkids.jwt.dto.JwtToken;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -39,12 +40,16 @@ public class User extends BaseTimeEntity {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private List<RefreshToken> refreshTokens = new ArrayList<>();
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Push> pushes = new ArrayList<>();
+
     @Builder
-    private User(String email, String password, SnsType snsType, String phoneNumber) {
+    public User(String email, String password, SnsType snsType, String phoneNumber, Role role) {
         this.email = email;
         this.password = password;
         this.snsType = snsType;
         this.phoneNumber = phoneNumber;
+        this.role = role;
     }
 
     public void checkSnsType(){
@@ -61,7 +66,7 @@ public class User extends BaseTimeEntity {
 
     /*
     * 비밀번호 체크
-    * */
+    */
     public void checkPassword(String password){
         if(!this.password.equals(password)){
             throw new LuckKidsException(ErrorCode.USER_PASSWORD);
@@ -70,25 +75,57 @@ public class User extends BaseTimeEntity {
 
     /*
     * List<RefreshToken>에 요청값으로 받은 deviceId와 일치하는 기존 RefreshToken이 있는지 조회 후 수정 혹은 등록한다.
-    * */
+    */
     public void checkRefreshToken(JwtToken jwtToken, String deviceId) {
+        //LAZY속성이기 때문에 사실 refreshToken이 select되지 않았기 때문에 초기화 시켜줌
         Hibernate.initialize(refreshTokens);
+
         // deviceId와 일치하는 RefreshToken 찾기
         RefreshToken existToken = refreshTokens.stream()
             .filter(refreshToken -> deviceId.equals(refreshToken.getDeviceId()))
             .findFirst()
             .orElse(null);
 
+        // deviceId와 일치하는 RefreshToken이 이미 존재하는 경우, 해당 토큰 업데이트
         if (existToken != null) {
-            // deviceId와 일치하는 RefreshToken이 이미 존재하는 경우, 해당 토큰 업데이트
             existToken.updateRefreshToken(jwtToken.getRefreshToken());
-        } else {
-            // deviceId와 일치하는 RefreshToken이 없는 경우, 새로운 RefreshToken 생성
-            RefreshToken newToken = new RefreshToken(this, jwtToken.getRefreshToken(), deviceId);
-            // 새로운 RefreshToken을 저장
-            refreshTokens.add(newToken);
+        }
+        else {
+            /*
+             * deviceId와 일치하는 RefreshToken이 없는 경우, 새로운 RefreshToken 생성 후
+             * 새로운 RefreshToken을 저장
+             */
+            refreshTokens.add(
+                RefreshToken.of(this,jwtToken.getRefreshToken(),deviceId)
+            );
         }
     }
+
+    public void checkPushKey(String pushToken, String deviceId) {
+        //LAZY속성이기 때문에 사실 Push가 select되지 않았기 때문에 초기화 시켜줌
+        Hibernate.initialize(pushes);
+
+        // deviceId와 일치하는 Push 찾기
+        Push existPush = pushes.stream()
+            .filter(push -> deviceId.equals(push.getDeviceId()))
+            .findFirst()
+            .orElse(null);
+
+        // deviceId와 일치하는 Push가 이미 존재하는 경우, 해당 PushToken 업데이트
+        if (existPush != null) {
+            existPush.updatePushToken(pushToken);
+        }
+        else {
+            /*
+             * deviceId와 일치하는 Push가 없는 경우, 새로운 Push 생성 후
+             * Push리스트에 add
+             */
+            pushes.add(
+                Push.of(this,pushToken,deviceId)
+            );
+        }
+    }
+
     /**
      * @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
      * private List<Mission> missions = new ArrayList<>();
@@ -98,8 +135,6 @@ public class User extends BaseTimeEntity {
      * private List<Friend> friends = new ArrayList<>();
      * @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
      * private List<AlertHistory> alertHistories = new ArrayList<>();
-     * @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
-     * private List<Push> pushes = new ArrayList<>();
      **/
 
 }
