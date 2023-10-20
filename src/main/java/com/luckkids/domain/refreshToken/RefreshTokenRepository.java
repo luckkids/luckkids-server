b0ac0d60e6b92fbb22e252eccea950bfd323523f
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-10-20 09:09:17 +0900

    fix: entity builder 추가

---
+++ src/main/java/com/luckkids/domain/refreshToken/RefreshTokenRepository.java
@@ -0,0 +1,8 @@
+package com.luckkids.domain.refreshToken;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Integer> {
+}
