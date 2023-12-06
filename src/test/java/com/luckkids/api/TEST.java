609a869bbfa908e099d3c86c9f3c3de335d9572a
HOYA <als77770@gmail.com>
2023-12-06 14:39:39 +0900

    test: 배포 실패에 대한 기능 추가

---
+++ src/test/java/com/luckkids/api/TEST.java
@@ -0,0 +1,19 @@
+package com.luckkids.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TEST {
+    @DisplayName("실패 테스트")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+        assertThat(1).isEqualTo(2);
+    }
+}
