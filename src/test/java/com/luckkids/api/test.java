62f643d0a785f6b8bc316c15abcae6f388b431fe
HOYA <als77770@gmail.com>
2023-12-07 10:06:17 +0900

    Create test.java

---
+++ src/test/java/com/luckkids/api/test.java
@@ -0,0 +1,19 @@
+package com.luckkids.api;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class test {
+
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+        Assertions.assertThat(1).isEqualTo(2);
+    }
+}
