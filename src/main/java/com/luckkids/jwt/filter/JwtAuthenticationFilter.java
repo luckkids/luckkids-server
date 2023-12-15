6c559eec0682a61e0d15e72d41f9dff727942118
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2023-12-15 09:44:16 +0900

    fix: getServletPath로 변경
    Context-path가 설정이 되었어도 문제가 없게끔 수정
    
    아래와 같은 주소가 있을 경우
    http://localhost:8080/test/index.jsp 
     
    request.getRequestURI();   //프로젝트경로부터 파일까지의 경로값을 얻어옴 (/test/index.jsp)
    request.getContextPath();  //프로젝트의 경로값만 가져옴(/test)
    request.getRequestURL();  //전체 경로를 가져옴 (http://localhost:8080/test/index.jsp)
    request.getServletPath();  //파일명 (/index.jsp)

--- src/main/java/com/luckkids/jwt/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/luckkids/jwt/filter/JwtAuthenticationFilter.java
@@ -37,7 +37,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Override
     protected boolean shouldNotFilter(HttpServletRequest request) {
-        String path = request.getRequestURI();
+        String path = request.getServletPath();
         return Arrays.stream(excludePath).anyMatch(path::startsWith);
     }
 
