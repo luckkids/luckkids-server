acdb386525f75e1df5ef19abdf01f50f0516448d
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-12-18 13:37:11 +0900

    fix: new url 불필요해보여 제거

--- src/test/java/com/luckkids/api/controller/initialSetting/initialSettingControllerTest.java
+++ src/test/java/com/luckkids/api/controller/initialSetting/initialSettingControllerTest.java
@@ -5,6 +5,7 @@ import com.luckkids.api.controller.initialSetting.request.InitialSettingAlertReq
 import com.luckkids.api.controller.initialSetting.request.InitialSettingCharacterRequest;
 import com.luckkids.api.controller.initialSetting.request.InitialSettingMissionRequest;
 import com.luckkids.api.controller.initialSetting.request.InitialSettingRequest;
+import com.luckkids.api.service.initialCharacter.response.InitialCharacterRandResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.security.test.context.support.WithMockUser;
@@ -17,6 +18,7 @@ import java.util.stream.IntStream;
 import static com.luckkids.domain.misson.AlertStatus.CHECKED;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -40,7 +42,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -58,7 +59,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -86,7 +87,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -104,7 +104,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -133,7 +133,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -151,7 +150,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -176,7 +175,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -193,7 +191,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -222,7 +220,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
         IntStream.rangeClosed(1, 10).forEach(i -> {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -240,7 +237,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -254,52 +251,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             .andExpect(jsonPath("$.data").isEmpty());
     }
 
-    @DisplayName("사용자의 초기세팅 데이터를 저장시 미션알림 여부는 필수이다.")
-    @Test
-    @WithMockUser("USER")
-    void createInitialSettingWithoutMissionAlertStatus() throws Exception {
-        // given
-        InitialSettingCharacterRequest initialSettingCharacterRequest = InitialSettingCharacterRequest.builder()
-            .fileName("test.json")
-            .characterName("럭키즈")
-            .build();
-
-        List<InitialSettingMissionRequest> initialSettingMissionRequests = new ArrayList<>();
-
-        IntStream.rangeClosed(1, 10).forEach(i -> {
-            initialSettingMissionRequests.add(
-                InitialSettingMissionRequest.builder()
-                    .missionDescription(i+"시에 운동하기")
-                    .alertTime(LocalTime.of(0,0))
-                    .build()
-            );
-        });
-
-        InitialSettingAlertRequest initialSettingAlertRequest = InitialSettingAlertRequest.builder()
-            .alertStatus(CHECKED)
-            .build();
-
-        InitialSettingRequest request = InitialSettingRequest.builder()
-            .character(initialSettingCharacterRequest)
-            .missions(initialSettingMissionRequests)
-            .alertSetting(initialSettingAlertRequest)
-            .build();
-
-        // when // then
-        mockMvc.perform(
-                post("/api/v1/initialSetting/new")
-                    .content(objectMapper.writeValueAsString(request))
-                    .contentType(APPLICATION_JSON)
-                    .accept(APPLICATION_JSON)
-                    .with(csrf())
-            )
-            .andDo(print())
-            .andExpect(status().isBadRequest())
-            .andExpect(jsonPath("$.statusCode").value("400"))
-            .andExpect(jsonPath("$.httpStatus").value("BAD_REQUEST"))
-            .andExpect(jsonPath("$.message").value("알람 여부는 필수입니다."))
-            .andExpect(jsonPath("$.data").isEmpty());
-    }
 
     @DisplayName("사용자의 초기세팅 데이터를 저장시 미션알림시간은 필수이다.")
     @Test
@@ -317,7 +268,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .build()
             );
         });
@@ -334,7 +284,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -369,7 +319,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -399,7 +349,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -416,7 +365,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -446,7 +395,6 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             initialSettingMissionRequests.add(
                 InitialSettingMissionRequest.builder()
                     .missionDescription(i+"시에 운동하기")
-                    .alertStatus(CHECKED)
                     .alertTime(LocalTime.of(0,0))
                     .build()
             );
@@ -459,7 +407,7 @@ public class initialSettingControllerTest extends ControllerTestSupport {
 
         // when // then
         mockMvc.perform(
-                post("/api/v1/initialSetting/new")
+                post("/api/v1/initialSetting")
                     .content(objectMapper.writeValueAsString(request))
                     .contentType(APPLICATION_JSON)
                     .accept(APPLICATION_JSON)
@@ -471,5 +419,35 @@ public class initialSettingControllerTest extends ControllerTestSupport {
             .andExpect(jsonPath("$.httpStatus").value("BAD_REQUEST"))
             .andExpect(jsonPath("$.message").value("알림설정 요청값은 필수입니다."))
             .andExpect(jsonPath("$.data").isEmpty());
+    }
+
+    @DisplayName("초기 캐릭터를 모두 조회한다.")
+    @Test
+    @WithMockUser("USER")
+    void findAllInitialCharacter() throws Exception {
+        // given
+        List<InitialCharacterRandResponse> initialCharacterRandResponses = new ArrayList<>();
+
+        IntStream.rangeClosed(1, 10).forEach(i -> {
+            initialCharacterRandResponses.add(
+                InitialCharacterRandResponse.builder()
+                    .characterName("테스트"+i)
+                    .fileName("test"+i+".json")
+                    .build()
+            );
+        });
+
+        // when // then
+        mockMvc.perform(
+                get("/api/v1/initialSetting/character")
+                    .contentType(APPLICATION_JSON)
+                    .accept(APPLICATION_JSON)
+                    .with(csrf())
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.statusCode").value("200"))
+            .andExpect(jsonPath("$.httpStatus").value("OK"))
+            .andExpect(jsonPath("$.message").value("OK"));
     }
 }
