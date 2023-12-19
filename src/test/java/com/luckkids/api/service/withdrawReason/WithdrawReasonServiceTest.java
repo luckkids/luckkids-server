95217e33b1179aa05913c8eaee1d7dff8a256a9b
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-12-19 14:39:39 +0900

    feat: 탈퇴사유저장 테스트코드

---
+++ src/test/java/com/luckkids/api/service/withdrawReason/WithdrawReasonServiceTest.java
@@ -0,0 +1,36 @@
+package com.luckkids.api.service.withdrawReason;
+
+import com.luckkids.IntegrationTestSupport;
+import com.luckkids.api.service.withdrawReason.request.WithdrawReasonCreateServiceRequest;
+import com.luckkids.api.service.withdrawReason.response.WithdrawReasonCreateResponse;
+import com.luckkids.domain.withdrawReason.WithdrawReason;
+import com.luckkids.domain.withdrawReason.WithdrawReasonRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WithdrawReasonServiceTest extends IntegrationTestSupport {
+
+    @Autowired
+    private WithdrawReasonRepository withdrawReasonRepository;
+    @Autowired
+    private WithdrawReasonService withdrawReasonService;
+
+    @Test
+    @DisplayName("탈퇴사유를 저장한다.")
+    void withdrawReasonTest(){
+        WithdrawReasonCreateServiceRequest withdrawReasonCreateServiceRequest = WithdrawReasonCreateServiceRequest.builder()
+            .reason("잘 사용하지 않는 앱이예요.")
+            .build();
+
+        WithdrawReasonCreateResponse withdrawReasonCreateResponse = withdrawReasonService.create(withdrawReasonCreateServiceRequest);
+
+        WithdrawReason savedWithdrawReason = withdrawReasonRepository.findById(withdrawReasonCreateResponse.getId()).get();
+
+        assertThat(savedWithdrawReason.getReason()).isEqualTo("잘 사용하지 않는 앱이예요.");
+
+    }
+}
