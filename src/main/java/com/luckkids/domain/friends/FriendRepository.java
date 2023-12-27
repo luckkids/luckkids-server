fb4019eea45f08a17d75984ebbe6e10e16d5de97
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2023-12-27 10:22:14 +0900

    feat: Entity 삭제메소드

--- src/main/java/com/luckkids/domain/friends/FriendRepository.java
+++ src/main/java/com/luckkids/domain/friends/FriendRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FriendRepository extends JpaRepository<Friend, Long>, FriendQueryRepository {
 
+    void deleteAllByReceiverId(int requestId);
+    void deleteAllByRequesterId(int receiverId);
 
 }
