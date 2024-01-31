958b2b05afe1b294d6ec615de601680d858adb13
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2024-01-31 10:20:33 +0900

    fix: kakaoOAuth Response수정

--- src/main/java/com/luckkids/api/feign/kakao/response/KakaoUserInfoResponse.java
+++ src/main/java/com/luckkids/api/feign/kakao/response/KakaoUserInfoResponse.java
@@ -1,5 +1,6 @@
 package com.luckkids.api.feign.kakao.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
@@ -8,29 +9,51 @@ import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class KakaoUserInfoResponse {
 
+    private long id;
+
+    @JsonProperty("connected_at")
+    private String connectedAt;
+
+    @JsonProperty("kakao_account")
     private KakaoAccount kakaoAccount;
 
-    @Getter
-    @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
     public static class KakaoAccount {
+
+        @JsonProperty("has_email")
+        private boolean hasEmail;
+
+        @JsonProperty("email_needs_agreement")
+        private boolean emailNeedsAgreement;
+
+        @JsonProperty("is_email_valid")
+        private boolean isEmailValid;
+
+        @JsonProperty("is_email_verified")
+        private boolean isEmailVerified;
+
+        @JsonProperty("email")
         private String email;
 
         @Builder
-        public KakaoAccount(String email) {
+        private KakaoAccount(boolean hasEmail, boolean emailNeedsAgreement, boolean isEmailValid, boolean isEmailVerified, String email) {
+            this.hasEmail = hasEmail;
+            this.emailNeedsAgreement = emailNeedsAgreement;
+            this.isEmailValid = isEmailValid;
+            this.isEmailVerified = isEmailVerified;
             this.email = email;
         }
     }
 
     @Builder
-    public KakaoUserInfoResponse(KakaoAccount kakaoAccount) {
+    private KakaoUserInfoResponse(long id, String connectedAt, KakaoAccount kakaoAccount) {
+        this.id = id;
+        this.connectedAt = connectedAt;
         this.kakaoAccount = kakaoAccount;
     }
 
     public String getEmail() {
         return kakaoAccount.email;
     }
-
 }
