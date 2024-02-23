c311c7e1d633a4b402d02c35bb996b22b6f63e7a
HOYA <102270909+mingeonho1@users.noreply.github.com>
2024-02-23 13:28:28 +0900

    feat: garden 리그 조회 (#140)
    * feat: garden 리그 조회
    
    * fix: delete friends

--- src/main/java/com/luckkids/domain/user/UserQueryRepository.java
+++ src/main/java/com/luckkids/domain/user/UserQueryRepository.java
@@ -1,11 +1,15 @@
 package com.luckkids.domain.user;
 
 import com.luckkids.domain.user.projection.MyProfileDto;
+import com.luckkids.domain.user.projection.UserLeagueDto;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+import static com.luckkids.domain.luckkidsCharacter.QLuckkidsCharacter.luckkidsCharacter;
 import static com.luckkids.domain.user.QUser.user;
 import static com.luckkids.domain.userCharacter.QUserCharacter.userCharacter;
 
@@ -21,12 +25,28 @@ public class UserQueryRepository {
                 user.id,
                 user.nickname,
                 user.luckPhrase,
-                userCharacter.luckkidsCharacter.imageFile,
+                luckkidsCharacter.imageFile,
                 user.missionCount
             ))
             .from(user)
             .join(user.userCharacter, userCharacter)
+            .join(userCharacter.luckkidsCharacter, luckkidsCharacter)
             .where(user.id.eq(userId))
             .fetchOne();
+    }
+
+    public List<UserLeagueDto> getUserLeagueTop3() {
+        return jpaQueryFactory
+            .select(Projections.constructor(UserLeagueDto.class,
+                user.nickname,
+                luckkidsCharacter.imageFile,
+                user.missionCount
+            ))
+            .from(user)
+            .join(user.userCharacter, userCharacter)
+            .join(userCharacter.luckkidsCharacter, luckkidsCharacter)
+            .orderBy(user.missionCount.desc())
+            .limit(3)
+            .fetch();
     }
 }
