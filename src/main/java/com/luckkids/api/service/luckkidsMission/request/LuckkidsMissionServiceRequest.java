a1a6fe54e79bb405ef79372e850e5c2c04736e1e
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2024-02-27 17:20:10 +0900

    fix: 럭키즈 습관 등록 API

---
+++ src/main/java/com/luckkids/api/service/luckkidsMission/request/LuckkidsMissionServiceRequest.java
@@ -0,0 +1,47 @@
+package com.luckkids.api.service.luckkidsMission.request;
+
+import com.luckkids.api.service.push.request.SendPushAlertTypeServiceRequest;
+import com.luckkids.domain.alertSetting.AlertType;
+import com.luckkids.domain.luckkidsMission.LuckkidsMission;
+import com.luckkids.domain.misson.MissionType;
+import com.luckkids.domain.push.PushMessage;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+public class LuckkidsMissionServiceRequest {
+
+    private MissionType missionType;
+
+    private String description;
+
+    private LocalTime alertTime;
+
+    @Builder
+    private LuckkidsMissionServiceRequest(MissionType missionType, String description, LocalTime alertTime) {
+        this.missionType = missionType;
+        this.description = description;
+        this.alertTime = alertTime;
+    }
+
+    public SendPushAlertTypeServiceRequest toSendPushAlertTypeRequest(AlertType alertType){
+        return SendPushAlertTypeServiceRequest.builder()
+            .screenName("MISSION")
+            .body(PushMessage.MISSION.getText())
+            .alertType(alertType)
+            .build();
+    }
+
+    public LuckkidsMission toEntity(){
+        return LuckkidsMission.builder()
+            .missionType(missionType)
+            .description(description)
+            .alertTime(alertTime)
+            .build();
+    }
+
+}
---
+++ src/main/java/com/luckkids/api/service/luckkidsMission/response/LuckkidsMissionSaveResponse.java
@@ -0,0 +1,39 @@
+package com.luckkids.api.service.luckkidsMission.response;
+
+import com.luckkids.domain.luckkidsMission.LuckkidsMission;
+import com.luckkids.domain.misson.MissionType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Getter
+@NoArgsConstructor
+public class LuckkidsMissionSaveResponse {
+
+    private int id;
+
+    private MissionType missionType;
+
+    private String description;
+
+    private LocalTime alertTime;
+
+    @Builder
+    private LuckkidsMissionSaveResponse(int id, MissionType missionType, String description, LocalTime alertTime) {
+        this.id = id;
+        this.missionType = missionType;
+        this.description = description;
+        this.alertTime = alertTime;
+    }
+
+    public static LuckkidsMissionSaveResponse of(LuckkidsMission luckkidsMission){
+        return LuckkidsMissionSaveResponse.builder()
+            .id(luckkidsMission.getId())
+            .missionType(luckkidsMission.getMissionType())
+            .description(luckkidsMission.getDescription())
+            .alertTime(luckkidsMission.getAlertTime())
+            .build();
+    }
+}
