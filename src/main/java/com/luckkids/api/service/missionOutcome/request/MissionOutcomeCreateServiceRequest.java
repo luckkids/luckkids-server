2b81723cbaac28012f2d1ff88a6aa4fa3cffb8b5
HOYA <als77770@gmail.com>
2024-03-21 12:06:20 +0900

    fix: successChecked 에러 수정

--- src/main/java/com/luckkids/api/service/missionOutcome/request/MissionOutcomeCreateServiceRequest.java
+++ src/main/java/com/luckkids/api/service/missionOutcome/request/MissionOutcomeCreateServiceRequest.java
@@ -1,34 +1,37 @@
 package com.luckkids.api.service.missionOutcome.request;
 
-import com.luckkids.domain.missionOutcome.MissionOutcome;
-import com.luckkids.domain.misson.Mission;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import static com.luckkids.domain.missionOutcome.MissionStatus.*;
+import static com.luckkids.domain.missionOutcome.SuccessChecked.*;
 
 import java.time.LocalDate;
 
-import static com.luckkids.domain.missionOutcome.MissionStatus.FAILED;
+import com.luckkids.domain.missionOutcome.MissionOutcome;
+import com.luckkids.domain.misson.Mission;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class MissionOutcomeCreateServiceRequest {
 
-    private Mission mission;
-    private LocalDate missionDate;
+	private Mission mission;
+	private LocalDate missionDate;
 
-    @Builder
-    private MissionOutcomeCreateServiceRequest(Mission mission, LocalDate missionDate) {
-        this.mission = mission;
-        this.missionDate = missionDate;
-    }
+	@Builder
+	private MissionOutcomeCreateServiceRequest(Mission mission, LocalDate missionDate) {
+		this.mission = mission;
+		this.missionDate = missionDate;
+	}
 
-    public MissionOutcome toEntity() {
-        return MissionOutcome.builder()
-            .mission(mission)
-            .missionStatus(FAILED)
-            .missionDate(missionDate)
-            .build();
-    }
+	public MissionOutcome toEntity() {
+		return MissionOutcome.builder()
+			.mission(mission)
+			.missionStatus(FAILED)
+			.missionDate(missionDate)
+			.successChecked(UNCHECKED)
+			.build();
+	}
 
 }
