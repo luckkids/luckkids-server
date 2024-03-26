9baf37d42d3b71dc601ef414b512fe6f953bf2b2
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2024-03-26 17:28:38 +0900

    fix: 미사용 메소드 제거

--- src/main/java/com/luckkids/domain/push/PushRepository.java
+++ src/main/java/com/luckkids/domain/push/PushRepository.java
@@ -9,7 +9,5 @@ import java.util.Optional;
 public interface PushRepository extends JpaRepository<Push, String> {
     void deleteAllByUserId(int userId);
     Optional<Push> findByDeviceIdAndUserId(String deviceId, int userId);
-    List<Push> findByAlertSetting_Notice(AlertStatus alertStatus);
-    List<Push> findByAlertSetting_Mission(AlertStatus alertStatus);
-    List<Push> findByAlertSetting_LuckMessage(AlertStatus alertStatus);
+    
 }
