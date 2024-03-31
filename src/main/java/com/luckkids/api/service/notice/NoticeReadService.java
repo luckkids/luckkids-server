97407771dd566ba562748e8b29b774515ba8a295
SangkiHan <68369248+SangkiHan@users.noreply.github.com>
2024-03-31 18:36:18 +0900

    fix: 공지사항 상세 API 제거, 공지사항 description -> url로 변경

--- src/main/java/com/luckkids/api/service/notice/NoticeReadService.java
+++ src/main/java/com/luckkids/api/service/notice/NoticeReadService.java
@@ -2,12 +2,9 @@ package com.luckkids.api.service.notice;
 
 import com.luckkids.api.exception.ErrorCode;
 import com.luckkids.api.exception.LuckKidsException;
-import com.luckkids.api.service.mission.response.MissionResponse;
-import com.luckkids.api.service.notice.response.NoticeDetailResponse;
 import com.luckkids.api.service.notice.response.NoticeResponse;
 import com.luckkids.domain.notice.Notice;
 import com.luckkids.domain.notice.NoticeRepository;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -23,15 +20,8 @@ public class NoticeReadService {
 
     private final NoticeRepository noticeRepository;
 
-    public NoticeDetailResponse getNotice(int id){
-        Notice notice = noticeRepository.findById(id)
-                .orElseThrow(()-> new LuckKidsException(ErrorCode.ALERT_UNKNOWN));
-
-        return NoticeDetailResponse.of(notice);
-    }
-
     public List<NoticeResponse> getNoticeList(){
-        List<Notice> noticeList = noticeRepository.findAll(Sort.by(Sort.Direction.DESC, "createdDate"));
-        return noticeList.stream().map(NoticeResponse::of).collect(Collectors.toList());
+        return noticeRepository.findAll(Sort.by(Sort.Direction.DESC, "createdDate"))
+                .stream().map(NoticeResponse::of).collect(Collectors.toList());
     }
 }
