7a9cf0e6702b96490cb4d0d75af0f33859ae0531
HOYA <als77770@gmail.com>
2024-04-20 15:24:57 +0900

    feat: cors 설정

--- src/main/java/com/luckkids/config/WebConfig.java
+++ src/main/java/com/luckkids/config/WebConfig.java
@@ -1,32 +1,45 @@
 package com.luckkids.config;
 
-import com.luckkids.config.interceptor.LoggingInterceptor;
-import com.luckkids.config.login.LoginUserArgumentResolver;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
+import com.luckkids.config.interceptor.LoggingInterceptor;
+import com.luckkids.config.login.LoginUserArgumentResolver;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-    private final LoginUserArgumentResolver loginUserArgumentResolver;
-    private final LoggingInterceptor loggingInterceptor;
+	private final LoginUserArgumentResolver loginUserArgumentResolver;
+	private final LoggingInterceptor loggingInterceptor;
 
-    /*
-    * LoginUserArgumentResolver 등록
-    * */
-    @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
-        argumentResolvers.add(loginUserArgumentResolver);
-    }
+	/*
+	 * LoginUserArgumentResolver 등록
+	 * */
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(loginUserArgumentResolver);
+	}
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loggingInterceptor)
-            .addPathPatterns("/**");
-    }
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(loggingInterceptor)
+			.addPathPatterns("/**");
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("GET", "POST", "PUT", "DELETE")
+			.allowedHeaders("Authorization", "Content-Type")
+			.allowCredentials(true)
+			.maxAge(3600);
+	}
 }
