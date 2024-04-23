3e1ea7efab2428cb23d4e6f255dd3014ea8cb60c
HOYA <als77770@gmail.com>
2024-04-23 14:20:42 +0900

    hotfix: cors 설정

--- src/main/java/com/luckkids/config/WebConfig.java
+++ src/main/java/com/luckkids/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.luckkids.config.login.LoginUserArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -28,5 +29,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loggingInterceptor)
             .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*");
     }
 }
