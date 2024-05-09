0a471b139792caa237a3c2253c48260b662b9dc4
HOYA <102270909+mingeonho1@users.noreply.github.com>
2024-05-09 16:37:28 +0900

    feat: 로그인한 유저 정보 조회 (#162)

--- src/main/java/com/luckkids/api/service/user/UserReadService.java
+++ src/main/java/com/luckkids/api/service/user/UserReadService.java
@@ -1,55 +1,63 @@
 package com.luckkids.api.service.user;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.luckkids.api.exception.ErrorCode;
 import com.luckkids.api.exception.LuckKidsException;
 import com.luckkids.api.service.security.SecurityService;
 import com.luckkids.api.service.user.request.UserFindEmailServiceRequest;
 import com.luckkids.api.service.user.response.UserFindSnsTypeResponse;
 import com.luckkids.api.service.user.response.UserLeagueResponse;
+import com.luckkids.api.service.user.response.UserResponse;
 import com.luckkids.domain.user.User;
 import com.luckkids.domain.user.UserQueryRepository;
 import com.luckkids.domain.user.UserRepository;
 import com.luckkids.domain.user.projection.UserLeagueDto;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class UserReadService {
 
-    private final UserRepository userRepository;
-    private final UserQueryRepository userQueryRepository;
-    private final SecurityService securityService;
+	private final UserRepository userRepository;
+	private final UserQueryRepository userQueryRepository;
+	private final SecurityService securityService;
 
-    public User findByOne(int id) {
-        return userRepository.findById(id)
-            .orElseThrow(() -> new IllegalArgumentException("해당 유저는 없습니다. id = " + id));
-    }
+	public User findByOne(int id) {
+		return userRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 유저는 없습니다. id = " + id));
+	}
 
-    public User findByEmail(String email) {
-        return userRepository.findByEmail(email)
-            .orElseThrow(() -> new LuckKidsException(ErrorCode.USER_EMAIL));
-    }
+	public UserResponse findByMe() {
+		int userId = securityService.getCurrentLoginUserInfo().getUserId();
+		return UserResponse.of(this.findByOne(userId));
+	}
 
-    public UserFindSnsTypeResponse findEmail(UserFindEmailServiceRequest userFindEmailServiceRequest) {
-        return UserFindSnsTypeResponse.of(findByEmail(userFindEmailServiceRequest.getEmail()));
-    }
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email)
+			.orElseThrow(() -> new LuckKidsException(ErrorCode.USER_EMAIL));
+	}
 
-    public List<UserLeagueResponse> getUserLeagueTop3() {
-        return userQueryRepository.getUserLeagueTop3()
-            .stream()
-            .map(UserLeagueDto::toUserLeagueResponse)
-            .collect(Collectors.toList());
-    }
+	public UserFindSnsTypeResponse findEmail(UserFindEmailServiceRequest userFindEmailServiceRequest) {
+		return UserFindSnsTypeResponse.of(findByEmail(userFindEmailServiceRequest.getEmail()));
+	}
 
-    public double getCharacterAchievementRate() {
-        int userId = securityService.getCurrentLoginUserInfo().getUserId();
-        User user = this.findByOne(userId);
-        return user.calculateAchievementRate();
-    }
+	public List<UserLeagueResponse> getUserLeagueTop3() {
+		return userQueryRepository.getUserLeagueTop3()
+			.stream()
+			.map(UserLeagueDto::toUserLeagueResponse)
+			.collect(Collectors.toList());
+	}
+
+	public double getCharacterAchievementRate() {
+		int userId = securityService.getCurrentLoginUserInfo().getUserId();
+		User user = this.findByOne(userId);
+		return user.calculateAchievementRate();
+	}
 }
