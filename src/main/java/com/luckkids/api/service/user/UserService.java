7a9155a33e7d39c969e3974bea7984b36387dd1c
HOYA <als77770@gmail.com>
2024-11-20 01:32:01 +0900

    fix: mission_active 해제시 미션 카운트 정합성 수정
    mission_active 해제시 만약 mission_outcome이 성공상태라면 user의 mission_count값 -1 아니면 그대로

--- src/main/java/com/luckkids/api/service/user/UserService.java
+++ src/main/java/com/luckkids/api/service/user/UserService.java
@@ -1,7 +1,20 @@
 package com.luckkids.api.service.user;
 
+import java.time.LocalDate;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luckkids.api.service.missionOutcome.MissionOutcomeReadService;
 import com.luckkids.api.service.security.SecurityService;
-import com.luckkids.api.service.user.delete.*;
+import com.luckkids.api.service.user.delete.UserAgreementDeleteService;
+import com.luckkids.api.service.user.delete.UserAlertDeleteService;
+import com.luckkids.api.service.user.delete.UserCharacterDeleteService;
+import com.luckkids.api.service.user.delete.UserFriendCodeDeleteService;
+import com.luckkids.api.service.user.delete.UserFriendDeleteService;
+import com.luckkids.api.service.user.delete.UserMissionDeleteService;
+import com.luckkids.api.service.user.delete.UserTokenDeleteService;
 import com.luckkids.api.service.user.request.UserUpdateLuckPhraseServiceRequest;
 import com.luckkids.api.service.user.request.UserUpdateNicknameServiceRequest;
 import com.luckkids.api.service.user.request.UserUpdatePasswordServiceRequest;
@@ -9,73 +22,82 @@ import com.luckkids.api.service.user.response.UserUpdateLuckPhraseResponse;
 import com.luckkids.api.service.user.response.UserUpdateNicknameResponse;
 import com.luckkids.api.service.user.response.UserUpdatePasswordResponse;
 import com.luckkids.api.service.user.response.UserWithdrawResponse;
+import com.luckkids.domain.missionOutcome.MissionStatus;
 import com.luckkids.domain.user.User;
 import com.luckkids.domain.user.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserService {
 
-    private final UserRepository userRepository;
+	private final UserRepository userRepository;
 
-    private final UserReadService userReadService;
-    private final SecurityService securityService;
+	private final UserReadService userReadService;
+	private final MissionOutcomeReadService missionOutcomeReadService;
+	private final SecurityService securityService;
 
-    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    private final UserCharacterDeleteService userCharacterDeleteService;
-    private final UserAlertDeleteService userAlertDeleteService;
-    private final UserTokenDeleteService userTokenDeleteService;
-    private final UserMissionDeleteService userMissionDeleteService;
-    private final UserFriendDeleteService userFriendDeleteService;
-    private final UserFriendCodeDeleteService userFriendCodeDeleteService;
-    private final UserAgreementDeleteService userAgreementDeleteService;
+	private final UserCharacterDeleteService userCharacterDeleteService;
+	private final UserAlertDeleteService userAlertDeleteService;
+	private final UserTokenDeleteService userTokenDeleteService;
+	private final UserMissionDeleteService userMissionDeleteService;
+	private final UserFriendDeleteService userFriendDeleteService;
+	private final UserFriendCodeDeleteService userFriendCodeDeleteService;
+	private final UserAgreementDeleteService userAgreementDeleteService;
 
-    public UserUpdatePasswordResponse updatePassword(UserUpdatePasswordServiceRequest userUpdatePasswordServiceRequest) {
-        User user = userReadService.findByEmail(userUpdatePasswordServiceRequest.getEmail());
-        user.updatePassword(bCryptPasswordEncoder.encode(userUpdatePasswordServiceRequest.getPassword()));
-        return UserUpdatePasswordResponse.of(user);
-    }
+	public UserUpdatePasswordResponse updatePassword(
+		UserUpdatePasswordServiceRequest userUpdatePasswordServiceRequest) {
+		User user = userReadService.findByEmail(userUpdatePasswordServiceRequest.getEmail());
+		user.updatePassword(bCryptPasswordEncoder.encode(userUpdatePasswordServiceRequest.getPassword()));
+		return UserUpdatePasswordResponse.of(user);
+	}
 
-    public UserUpdateLuckPhraseResponse updatePhrase(UserUpdateLuckPhraseServiceRequest userUpdateLuckPhraseServiceRequest) {
-        User user = getCurrentUser();
-        user.updateLuckPhrase(userUpdateLuckPhraseServiceRequest.getLuckPhrase());
-        return UserUpdateLuckPhraseResponse.of(user);
-    }
+	public UserUpdateLuckPhraseResponse updatePhrase(
+		UserUpdateLuckPhraseServiceRequest userUpdateLuckPhraseServiceRequest) {
+		User user = getCurrentUser();
+		user.updateLuckPhrase(userUpdateLuckPhraseServiceRequest.getLuckPhrase());
+		return UserUpdateLuckPhraseResponse.of(user);
+	}
 
-    public UserUpdateNicknameResponse updateNickname(UserUpdateNicknameServiceRequest userUpdateNicknameServiceRequest) {
-        User user = getCurrentUser();
-        String nickname = userUpdateNicknameServiceRequest.getNickname();
-        user.updateNickName(nickname);
-        return UserUpdateNicknameResponse.of(nickname);
-    }
+	public UserUpdateNicknameResponse updateNickname(
+		UserUpdateNicknameServiceRequest userUpdateNicknameServiceRequest) {
+		User user = getCurrentUser();
+		String nickname = userUpdateNicknameServiceRequest.getNickname();
+		user.updateNickName(nickname);
+		return UserUpdateNicknameResponse.of(nickname);
+	}
 
-    public UserWithdrawResponse withdraw() {
-        User user = getCurrentUser();
-        int userId = user.getId();
+	public void minusMissionCount(int missionId, LocalDate missionDate) {
+		MissionStatus missionOutcomeStatus = missionOutcomeReadService.getMissionOutcomeStatus(missionId, missionDate);
+		if (missionOutcomeStatus.equals(MissionStatus.SUCCEED)) {
+			User user = getCurrentUser();
+			user.minusMissionCount();
+		}
+	}
 
-        userCharacterDeleteService.deleteAllByUserId(userId);
-        userAlertDeleteService.deleteAllByUserId(userId);
-        userTokenDeleteService.deleteAllByUserId(userId);
-        userMissionDeleteService.deleteAllByUserId(userId);
-        userFriendDeleteService.deleteAllByUserId(userId);
-        userFriendCodeDeleteService.deleteAllByUserId(userId);
-        userAgreementDeleteService.deleteAllByUserId(userId);
-        userRepository.deleteById(userId);
+	public UserWithdrawResponse withdraw() {
+		User user = getCurrentUser();
+		int userId = user.getId();
 
-        return UserWithdrawResponse.of(userId);
-    }
+		userCharacterDeleteService.deleteAllByUserId(userId);
+		userAlertDeleteService.deleteAllByUserId(userId);
+		userTokenDeleteService.deleteAllByUserId(userId);
+		userMissionDeleteService.deleteAllByUserId(userId);
+		userFriendDeleteService.deleteAllByUserId(userId);
+		userFriendCodeDeleteService.deleteAllByUserId(userId);
+		userAgreementDeleteService.deleteAllByUserId(userId);
+		userRepository.deleteById(userId);
 
-    private User getCurrentUser() {
-        int userId = securityService.getCurrentLoginUserInfo().getUserId();
-        return userReadService.findByOne(userId);
-    }
+		return UserWithdrawResponse.of(userId);
+	}
+
+	private User getCurrentUser() {
+		int userId = securityService.getCurrentLoginUserInfo().getUserId();
+		return userReadService.findByOne(userId);
+	}
 
 }
