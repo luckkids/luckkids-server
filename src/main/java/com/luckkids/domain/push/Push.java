18bfd588122a9ac990c41b9f1d11af0dedd2b04a
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-01-20 22:07:02 +0900

    refactor: 코드컨벤션

--- src/main/java/com/luckkids/domain/push/Push.java
+++ src/main/java/com/luckkids/domain/push/Push.java
@@ -1,66 +1,70 @@
 package com.luckkids.domain.push;
 
 import com.luckkids.domain.BaseTimeEntity;
-import com.luckkids.domain.alertHistory.AlertHistory;
 import com.luckkids.domain.alertSetting.AlertSetting;
 import com.luckkids.domain.user.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Push extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    private String deviceId;
+	private String deviceId;
 
-    private String pushToken;
+	private String pushToken;
 
-    private String sound;
+	private String sound;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
-    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "push")
-    private AlertSetting alertSetting;
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "push")
+	private AlertSetting alertSetting;
 
-    @Builder
-    private Push(String deviceId, User user, String pushToken, String sound) {
-        this.deviceId = deviceId;
-        this.user = user;
-        this.pushToken = pushToken;
-        this.sound = sound;
-    }
+	@Builder
+	private Push(String deviceId, User user, String pushToken, String sound) {
+		this.deviceId = deviceId;
+		this.user = user;
+		this.pushToken = pushToken;
+		this.sound = sound;
+	}
 
-    public static Push of(String deviceId, User user, String pushToken) {
-        return Push.builder()
-            .deviceId(deviceId)
-            .user(user)
-            .pushToken(pushToken)
-            .sound(PushMessage.DEFAULT_SOUND.getText())
-            .build();
-    }
+	public static Push of(String deviceId, User user, String pushToken) {
+		return Push.builder()
+			.deviceId(deviceId)
+			.user(user)
+			.pushToken(pushToken)
+			.sound(PushMessage.DEFAULT_SOUND.getText())
+			.build();
+	}
 
-    public void updatePushToken(String pushToken) {
-        this.pushToken = pushToken;
-    }
+	public void updatePushToken(String pushToken) {
+		this.pushToken = pushToken;
+	}
 
-    public void updateUser(User user) {
-        this.user = user;
-        user.getPushes().add(this);
-    }
+	public void updateUser(User user) {
+		this.user = user;
+		user.getPushes().add(this);
+	}
 
-    public void updateSound(String sound){
-        this.sound = sound;
-    }
+	public void updateSound(String sound) {
+		this.sound = sound;
+	}
 }
