5e20464332c71e0eabf5836c498a24f7ccbf3960
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-11 11:15:59 +0900

    feat: 개발 GitAction yml, script (#244)

---
+++ scripts/dev/switch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+CURRENT_PORT=$(cat /etc/nginx/conf.d/service-url.inc | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
+
+if [ ${CURRENT_PORT} -eq 8777 ]; then
+    TARGET_PORT=8778
+elif [ ${CURRENT_PORT} -eq 8778 ]; then
+    TARGET_PORT=8777
+else
+    echo "> No WAS is connected to nginx"
+    exit 1
+fi
+
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+
+echo "> Now Nginx proxies to ${TARGET_PORT}."
+
+sudo service nginx reload
+
+echo "> Nginx reloaded."
+
+CURRENT_PID=$(lsof -Fp -i TCP:${CURRENT_PORT} | grep -Po 'p[0-9]+' | grep -Po '[0-9]+')
+
+sudo kill ${CURRENT_PID}
+
+echo "> kill ${CURRENT_PID}"
