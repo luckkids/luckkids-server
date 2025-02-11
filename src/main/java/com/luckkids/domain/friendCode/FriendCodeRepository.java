7e1bcc0dc1e2a93c62a77a91b47adbf5a66f461e
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-11 12:40:34 +0900

    feat: 배포테스트 (#248)

--- src/main/java/com/luckkids/domain/friendCode/FriendCodeRepository.java
+++ src/main/java/com/luckkids/domain/friendCode/FriendCodeRepository.java
@@ -1,11 +1,13 @@
 package com.luckkids.domain.friendCode;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface FriendCodeRepository extends JpaRepository<FriendCode, Long> {
-    boolean existsByCode(String code);
-    void deleteAllByUserId(int receiverId);
-    Optional<FriendCode> findByCode(String code);
+	boolean existsByCode(String code);
+
+	void deleteAllByUserId(int receiverId);
+
+	Optional<FriendCode> findByCode(String code);
 }
