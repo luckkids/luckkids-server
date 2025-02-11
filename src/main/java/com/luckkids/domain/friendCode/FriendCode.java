d7892ca33fb09a94829daa65b2443e6b0996bb19
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-11 11:52:24 +0900

    feat: 배포테스트 (#246)

--- src/main/java/com/luckkids/domain/friendCode/FriendCode.java
+++ src/main/java/com/luckkids/domain/friendCode/FriendCode.java
@@ -4,7 +4,15 @@ import com.luckkids.api.exception.ErrorCode;
 import com.luckkids.api.exception.LuckKidsException;
 import com.luckkids.domain.BaseTimeEntity;
 import com.luckkids.domain.user.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,30 +22,31 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class FriendCode extends BaseTimeEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
-    private String code;
+	private String code;
 
-    @Enumerated(EnumType.STRING)
-    private UseStatus useStatus;
+	@Enumerated(EnumType.STRING)
+	private UseStatus useStatus;
 
-    @Builder
-    private FriendCode(User user, String code, UseStatus useStatus) {
-        this.user = user;
-        this.code = code;
-        this.useStatus = useStatus;
-    }
+	@Builder
+	private FriendCode(User user, String code, UseStatus useStatus) {
+		this.user = user;
+		this.code = code;
+		this.useStatus = useStatus;
+	}
 
-    public void checkUsed(){
-        if(useStatus.equals(UseStatus.USED)) throw new LuckKidsException(ErrorCode.FRIEND_CODE_USED);
-    }
+	public void checkUsed() {
+		if (useStatus.equals(UseStatus.USED))
+			throw new LuckKidsException(ErrorCode.FRIEND_CODE_USED);
+	}
 
-    public void updateUseStatus(){
-        this.useStatus = UseStatus.USED;
-    }
+	public void updateUseStatus() {
+		this.useStatus = UseStatus.USED;
+	}
 }
