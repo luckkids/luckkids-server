046c554dc8ee3b317745e7f3322b130df7dbbfe4
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-11 16:30:48 +0900

    fix: 원복

--- src/main/java/com/luckkids/domain/refreshToken/RefreshToken.java
+++ src/main/java/com/luckkids/domain/refreshToken/RefreshToken.java
@@ -3,7 +3,12 @@ package com.luckkids.domain.refreshToken;
 import com.luckkids.domain.BaseTimeEntity;
 import com.luckkids.domain.user.User;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
