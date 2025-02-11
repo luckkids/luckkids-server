4e24d30afcd1a1110ed2f611faa6d6db91af86b2
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-11 16:32:36 +0900

    feat: 배포테스트 (#257)
    * Update RefreshToken.java
    
    * fix: 원복

--- src/main/java/com/luckkids/domain/refreshToken/RefreshToken.java
+++ src/main/java/com/luckkids/domain/refreshToken/RefreshToken.java
@@ -2,7 +2,13 @@ package com.luckkids.domain.refreshToken;
 
 import com.luckkids.domain.BaseTimeEntity;
 import com.luckkids.domain.user.User;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,38 +19,38 @@ import lombok.NoArgsConstructor;
 @Entity
 public class RefreshToken extends BaseTimeEntity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
 
-    private String deviceId;
+	private String deviceId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    private User user;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
-    private String refreshToken;
+	private String refreshToken;
 
-    @Builder
-    private RefreshToken(User user, String refreshToken, String deviceId){
-        this.user = user;
-        this.refreshToken = refreshToken;
-        this.deviceId = deviceId;
-    }
+	@Builder
+	private RefreshToken(User user, String refreshToken, String deviceId) {
+		this.user = user;
+		this.refreshToken = refreshToken;
+		this.deviceId = deviceId;
+	}
 
-    public static RefreshToken of(User user, String refreshToken, String deviceId){
-        return RefreshToken.builder()
-            .user(user)
-            .refreshToken(refreshToken)
-            .deviceId(deviceId)
-            .build();
-    }
+	public static RefreshToken of(User user, String refreshToken, String deviceId) {
+		return RefreshToken.builder()
+			.user(user)
+			.refreshToken(refreshToken)
+			.deviceId(deviceId)
+			.build();
+	}
 
-    public void updateRefreshToken(String refreshToken){
-        this.refreshToken = refreshToken;
-    }
+	public void updateRefreshToken(String refreshToken) {
+		this.refreshToken = refreshToken;
+	}
 
-    public void setUser(User user){
-        this.user = user;
-        user.getRefreshTokens().add(this);
-    }
+	public void setUser(User user) {
+		this.user = user;
+		user.getRefreshTokens().add(this);
+	}
 }
