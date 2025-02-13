60b0eefdc542a51324889dd39958d173dd7f51e7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-02-13 12:13:46 +0900

    feat: 배포를 위한 PR

--- src/test/java/com/luckkids/api/service/version/VersionServiceTest.java
+++ src/test/java/com/luckkids/api/service/version/VersionServiceTest.java
@@ -1,41 +1,42 @@
 package com.luckkids.api.service.version;
 
-import com.luckkids.IntegrationTestSupport;
-import com.luckkids.api.service.version.request.VersionSaveServiceRequest;
-import com.luckkids.api.service.version.response.VersionSaveResponse;
-import com.luckkids.domain.version.Version;
-import com.luckkids.domain.version.VersionRepository;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import com.luckkids.IntegrationTestSupport;
+import com.luckkids.api.service.version.request.VersionSaveServiceRequest;
+import com.luckkids.api.service.version.response.VersionSaveResponse;
+import com.luckkids.domain.version.Version;
+import com.luckkids.domain.version.VersionRepository;
 
 class VersionServiceTest extends IntegrationTestSupport {
 
-    @Autowired
-    private VersionService versionService;
+	@Autowired
+	private VersionService versionService;
 
-    @Autowired
-    private VersionRepository versionRepository;
+	@Autowired
+	private VersionRepository versionRepository;
 
-    @AfterEach
-    void tearDown() {
-        versionRepository.deleteAllInBatch();
-    }
+	@AfterEach
+	void tearDown() {
+		versionRepository.deleteAllInBatch();
+	}
 
-    @DisplayName(value = "최신버전을 저장한다.")
-    @Test
-    void saveVersionTest() {
-        VersionSaveServiceRequest versionSaveServiceRequest = VersionSaveServiceRequest.builder()
-            .versionNum("1.1.2")
-            .build();
+	@DisplayName(value = "최신버전을 저장한다.")
+	@Test
+	void saveVersionTest() {
+		VersionSaveServiceRequest versionSaveServiceRequest = VersionSaveServiceRequest.builder()
+			.versionNum("1.1.2")
+			.build();
 
-        VersionSaveResponse versionSaveResponse = versionService.save(versionSaveServiceRequest);
+		VersionSaveResponse versionSaveResponse = versionService.save(versionSaveServiceRequest);
 
-        Version version = versionRepository.findById(versionSaveResponse.getId()).get();
+		Version version = versionRepository.findById(versionSaveResponse.getId()).get();
 
-        assertThat(version.getVersionNum()).isEqualTo("1.1.2");
-    }
+		assertThat(version.getVersionNum()).isEqualTo("1.1.2");
+	}
 }
