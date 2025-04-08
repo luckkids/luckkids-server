307ebad4326861e31b2f28ad5b33eee90b7d18bf
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-04-08 19:59:58 +0900

    fix: 오늘의 한마디 조회 param으로 변경

--- src/main/java/com/luckkids/api/controller/luckMessageHistory/request/LuckMessageHistoryRequest.java
+++ src/main/java/com/luckkids/api/controller/luckMessageHistory/request/LuckMessageHistoryRequest.java
@@ -6,8 +6,10 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class LuckMessageHistoryRequest {
 
