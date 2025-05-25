0f78a5550642df3799108158cb066472eee1dbea
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2025-05-25 15:51:04 +0900

    feat: 행운의한마디 삭제 기능

---
+++ src/main/java/com/luckkids/api/service/user/delete/UserLuckMessageHistoryDeleteService.java
@@ -0,0 +1,19 @@
+package com.luckkids.api.service.user.delete;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.luckkids.domain.luckMessageHistory.LuckMessageHistoryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserLuckMessageHistoryDeleteService {
+	private final LuckMessageHistoryRepository luckMessageHistoryRepository;
+
+	public void deleteAllByUserId(int userId) {
+		luckMessageHistoryRepository.deleteAllByUserId(userId);
+	}
+}
