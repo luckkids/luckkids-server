31d72d278f491ab2f4fbb8b9a879e2520d393428
HOYA <102270909+mingeonho1@users.noreply.github.com>
2026-01-10 18:32:27 +0900

    feat: 개운법 테스트 생성, 조회 (#278)

---
+++ src/test/java/com/luckkids/api/service/fortuneTestHistory/FortuneTestHistoryServiceTest.java
@@ -0,0 +1,54 @@
+package com.luckkids.api.service.fortuneTestHistory;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.luckkids.IntegrationTestSupport;
+import com.luckkids.api.service.fortuneTestHistory.request.FortuneTestHistoryCreateServiceRequest;
+import com.luckkids.api.service.fortuneTestHistory.response.FortuneTestHistoryResponse;
+import com.luckkids.domain.fortuneTestHistory.FortuneTestHistory;
+import com.luckkids.domain.fortuneTestHistory.FortuneTestHistoryRepository;
+import com.luckkids.domain.fortuneTestHistory.FortuneTestResultType;
+
+class FortuneTestHistoryServiceTest extends IntegrationTestSupport {
+
+	@Autowired
+	private FortuneTestHistoryService fortuneTestHistoryService;
+
+	@Autowired
+	private FortuneTestHistoryRepository fortuneTestHistoryRepository;
+
+	@AfterEach
+	void tearDown() {
+		fortuneTestHistoryRepository.deleteAllInBatch();
+	}
+
+	@DisplayName("닉네임과 운세결과를 받아 운세 테스트 결과를 저장한다.")
+	@Test
+	void createFortuneTestHistory() {
+		// given
+		FortuneTestHistoryCreateServiceRequest request = FortuneTestHistoryCreateServiceRequest.builder()
+			.nickname("럭키즈")
+			.resultType(FortuneTestResultType.A)
+			.build();
+
+		// when
+		FortuneTestHistoryResponse response = fortuneTestHistoryService.createFortuneTestHistory(request);
+
+		// then
+		assertThat(response)
+			.extracting("nickname", "resultType")
+			.contains("럭키즈", FortuneTestResultType.A);
+
+		List<FortuneTestHistory> histories = fortuneTestHistoryRepository.findAll();
+		assertThat(histories).hasSize(1)
+			.extracting("nickname", "resultType")
+			.containsExactly(tuple("럭키즈", FortuneTestResultType.A));
+	}
+}
