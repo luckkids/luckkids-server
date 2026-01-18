05e98b8f613285575a367b97460944cf20658cc7
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2026-01-18 10:17:37 +0900

    fix: 결과타입 수정

--- src/main/java/com/luckkids/domain/fortuneTestHistory/FortuneTestResultType.java
+++ src/main/java/com/luckkids/domain/fortuneTestHistory/FortuneTestResultType.java
@@ -1,5 +1,16 @@
 package com.luckkids.domain.fortuneTestHistory;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum FortuneTestResultType {
-	A, B, C, D, E
+	TOKKINGI("토끼이"),
+	TAEYANGI("태양이"),
+	NABY("네이피"),
+	TURKEYI("터키이"),
+	GURUMI("구르미");
+
+	private final String description;
 }
