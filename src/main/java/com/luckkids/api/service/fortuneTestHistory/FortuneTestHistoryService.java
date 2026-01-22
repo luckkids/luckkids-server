9896dc108cfa128c1e5c806ed5cb2870965ad689
SangkiHan <tkdrl8908@naver.com>
2026-01-22 17:05:12 +0900

    fix: 개운법 저장 uuid로 변경

--- src/main/java/com/luckkids/api/service/fortuneTestHistory/FortuneTestHistoryService.java
+++ src/main/java/com/luckkids/api/service/fortuneTestHistory/FortuneTestHistoryService.java
@@ -1,39 +1,20 @@
 package com.luckkids.api.service.fortuneTestHistory;
 
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.luckkids.api.service.fortuneTestHistory.request.FortuneTestHistoryCreateServiceRequest;
 import com.luckkids.api.service.fortuneTestHistory.response.FortuneTestHistoryResponse;
-import com.luckkids.domain.fortuneTestHistory.FortuneTestHistory;
 import com.luckkids.domain.fortuneTestHistory.FortuneTestHistoryRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class FortuneTestHistoryService {
 
-	private final FortuneTestHistoryRepository fortuneTestHistoryRepository;
+    private final FortuneTestHistoryRepository fortuneTestHistoryRepository;
 
-	public FortuneTestHistoryResponse createFortuneTestHistory(FortuneTestHistoryCreateServiceRequest request) {
-		FortuneTestHistory history = FortuneTestHistory.builder()
-			.nickname(request.getNickname())
-			.resultType(request.getResultType())
-			.build();
-
-		FortuneTestHistory savedHistory = fortuneTestHistoryRepository.save(history);
-
-		return FortuneTestHistoryResponse.of(savedHistory);
-	}
-
-	@Transactional(readOnly = true)
-	public List<FortuneTestHistoryResponse> findAll() {
-		return fortuneTestHistoryRepository.findAll().stream()
-			.map(FortuneTestHistoryResponse::of)
-			.toList();
-	}
+    public FortuneTestHistoryResponse createFortuneTestHistory(FortuneTestHistoryCreateServiceRequest request) {
+        return FortuneTestHistoryResponse.of(fortuneTestHistoryRepository.save(request.toEntity()));
+    }
 }
