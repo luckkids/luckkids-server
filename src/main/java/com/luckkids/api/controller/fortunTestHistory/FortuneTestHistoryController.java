ac05def1195428818eae49e5e5b5cf015c7c3763
SangkiHan <tkdrl8908@naver.com>
2026-01-22 17:05:46 +0900

    fix: 개운법 조회  API

--- src/main/java/com/luckkids/api/controller/fortunTestHistory/FortuneTestHistoryController.java
+++ src/main/java/com/luckkids/api/controller/fortunTestHistory/FortuneTestHistoryController.java
@@ -1,41 +1,31 @@
 package com.luckkids.api.controller.fortunTestHistory;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.luckkids.api.ApiResponse;
 import com.luckkids.api.controller.fortunTestHistory.request.FortuneTestHistoryCreateRequest;
 import com.luckkids.api.service.fortuneTestHistory.FortuneTestHistoryReadService;
 import com.luckkids.api.service.fortuneTestHistory.FortuneTestHistoryService;
 import com.luckkids.api.service.fortuneTestHistory.response.FortuneTestHistoryResponse;
-
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/fortuneTest")
 public class FortuneTestHistoryController {
 
-	private final FortuneTestHistoryService fortuneTestHistoryService;
-	private final FortuneTestHistoryReadService fortuneTestHistoryReadService;
+    private final FortuneTestHistoryService fortuneTestHistoryService;
+    private final FortuneTestHistoryReadService fortuneTestHistoryReadService;
 
-	@PostMapping
-	public ApiResponse<FortuneTestHistoryResponse> createFortuneTestHistory(
-		@Valid @RequestBody FortuneTestHistoryCreateRequest request) {
-		FortuneTestHistoryResponse response = fortuneTestHistoryService.createFortuneTestHistory(
-			request.toServiceRequest());
-		return ApiResponse.ok(response);
-	}
+    @PostMapping
+    public ApiResponse<FortuneTestHistoryResponse> createFortuneTestHistory(
+            @Valid @RequestBody FortuneTestHistoryCreateRequest request) {
+        return ApiResponse.ok(fortuneTestHistoryService.createFortuneTestHistory(
+                request.toServiceRequest()));
+    }
 
-	@GetMapping
-	public ApiResponse<List<FortuneTestHistoryResponse>> getFortuneTestHistories() {
-		List<FortuneTestHistoryResponse> response = fortuneTestHistoryService.findAll();
-		return ApiResponse.ok(response);
-	}
+    @GetMapping
+    public ApiResponse<FortuneTestHistoryResponse> findByUuid(String uuid) {
+        return ApiResponse.ok(fortuneTestHistoryReadService.findByUuid(uuid));
+    }
 }
