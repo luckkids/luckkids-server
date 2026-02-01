4dd0c5277fd57ee5e223cca946a5d0a8b094112a
HOYA <102270909+mingeonho1@users.noreply.github.com>
2026-02-01 22:39:53 +0900

    fix: cors설정 (#283)

--- src/main/java/com/luckkids/config/WebConfig.java
+++ src/main/java/com/luckkids/config/WebConfig.java
@@ -36,7 +36,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("luckkids-quiz.netlify.app")
+			.allowedOrigins("https://luckkids-quiz.netlify.app")
 			.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
 			.allowedHeaders("*")
 			.allowCredentials(true)
